28fd40463f924cd81bfe84923a50bd567066cc73
Kalash Nainwal <kalash@nexthop.ai>
2026-05-07 18:23:28 -0700

    [FRR] Enable building and running sharpd (#26738)
    * Enable building and running sharpd
    
    signed-off-by: Kalash Nainwal <kalash@nexthop.ai>
    Signed-off-by: Kalash Nainwal <kalash@nexthop.ai>
    
    * Addressing review comments- adding 'SONIC-ONLY' to the sonic specific patch, as is the convention
    
    Signed-off-by: Kalash Nainwal <kalash@nexthop.ai>
    
    * Addressing review comments
    
    Signed-off-by: Kalash Nainwal <kalash@nexthop.ai>
    
    ---------
    
    Signed-off-by: Kalash Nainwal <kalash@nexthop.ai>

--- dockers/docker-fpm-frr/docker_init.sh
+++ dockers/docker-fpm-frr/docker_init.sh
@@ -63,6 +63,7 @@ if [ -z "$CONFIG_TYPE" ] || [ "$CONFIG_TYPE" == "separated" ]; then
         -t /usr/share/sonic/templates/bgpd/gen_bgpd.conf.j2,/etc/frr/bgpd.conf \
         -t /usr/share/sonic/templates/zebra/zebra.conf.j2,/etc/frr/zebra.conf \
         -t /usr/share/sonic/templates/staticd/gen_staticd.conf.j2,/etc/frr/staticd.conf \
+        -t /usr/share/sonic/templates/sharpd/sharpd.conf.j2,/etc/frr/sharpd.conf \
     "
     MGMT_FRAMEWORK_CONFIG=$(echo $FRR_VARS | jq -r '.frr_mgmt_framework_config')
     if [ -n "$MGMT_FRAMEWORK_CONFIG" ] && [ "$MGMT_FRAMEWORK_CONFIG" != "false" ]; then
@@ -82,7 +83,8 @@ elif [ "$CONFIG_TYPE" == "split" ]; then
     write_default_zebra_config /etc/frr/zebra.conf
 elif [ "$CONFIG_TYPE" == "split-unified" ]; then
     echo "service integrated-vtysh-config" > /etc/frr/vtysh.conf
-    rm -f /etc/frr/bgpd.conf /etc/frr/zebra.conf /etc/frr/staticd.conf
+    rm -f /etc/frr/bgpd.conf /etc/frr/zebra.conf /etc/frr/staticd.conf \
+          /etc/frr/sharpd.conf
     write_default_zebra_config /etc/frr/frr.conf
 elif [ "$CONFIG_TYPE" == "unified" ]; then
     CFGGEN_PARAMS=" \
@@ -94,7 +96,8 @@ elif [ "$CONFIG_TYPE" == "unified" ]; then
     sonic-cfggen $CFGGEN_PARAMS
     echo "service integrated-vtysh-config" > /etc/frr/vtysh.conf
     rm -f /etc/frr/bgpd.conf /etc/frr/zebra.conf /etc/frr/staticd.conf \
-          /etc/frr/bfdd.conf /etc/frr/ospfd.conf /etc/frr/pimd.conf
+          /etc/frr/bfdd.conf /etc/frr/ospfd.conf /etc/frr/pimd.conf \
+          /etc/frr/sharpd.conf
 fi
 
 chown -R frr:frr /etc/frr/
